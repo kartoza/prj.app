dc7726105c8120a2a5dcf48e92752cbe27e9425f
Tim Sutton <tim@linfiniti.com>
2013-08-08 10:03:27 +0200

    Fabric deploy improvements (still in progress)

--- fabfile.py
+++ fabfile.py
@@ -7,7 +7,7 @@
 # pip install fabric fabtools
 
 import os
-from fabric.api import task
+from fabric.api import task, env, fastprint
 from fabtools.require import postfix
 from fabtools.deb import update_index
 # Don't remove even though its unused
@@ -17,7 +17,8 @@ from fabtools.vagrant import vagrant
 # noinspection PyUnresolvedReferences
 from fabgis.dropbox import setup_dropbox, setup_dropbox_daemon
 from fabgis.django import setup_apache
-
+from fabgis.utilities import update_git_checkout
+from fabgis.common import setup_env, show_environment
 
 @task
 def deploy():
@@ -31,8 +32,16 @@ def deploy():
     # Ensure we have a mailserver setup for our domain
     # Note that you may have problems if you intend to run more than one
     # site from the same server
+    setup_env()
+    show_environment()
     site_name = 'visual_changelog'
-    code_path = os.path.abspath(os.path.dirname(__file__))
+    base_path = os.path.abspath(os.path.join(
+        env.fg.home, 'dev', 'python'))
+    git_url = 'http://github.com/timlinux/visual_changelog.git'
+    repo_alias = 'visual_changelog'
+    fastprint('Checking out %s to %s as %s' % (git_url, base_path, repo_alias))
+    update_git_checkout(base_path, git_url, repo_alias)
+    code_path = os.path.abspath(os.path.join(base_path, repo_alias))
     update_index()
     postfix.server(site_name)
     setup_apache(site_name, code_path=code_path)
