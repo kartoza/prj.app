46aae9a65c0a995be795991ff9fcab514de502d4
Tim Sutton <tim@linfiniti.com>
2013-09-01 23:54:15 +0200

    In progress support for hashed image names.

--- django_project/changes/management/commands/purge_unused_images.py
+++ django_project/changes/management/commands/purge_unused_images.py
@@ -1,7 +1,10 @@
 # coding=utf-8
 """A command to get rid of any images that are not being actively used."""
 
+import os
+
 from django.core.management.base import BaseCommand, CommandError
+from django.core.files import File
 from ...models import Entry
 
 
@@ -17,17 +20,24 @@ class Command(BaseCommand):
         :param args:  Not used
         :param options: Not used
         """
-        try:
-            entries = Entry.all_objects.all()
-            for entry in entries:
-                # Force converting image to a hashed name
-                # this is to support image migrations of older versions
-                # of changelogger.
+        entries = Entry.all_objects.all()
+        for entry in entries:
+            # Force converting image to a hashed name
+            # this is to support image migrations of older versions
+            # of changelogger.
 
-                self.stdout.write(entry.title)
-                entry.save()
+            self.stdout.write(entry.title)
+            image_path = entry.image_file.name
+            if 'images/entries' in image_path:
+                self.stdout.write('Skipping %s' % image_path)
+                continue
+            else:
+                self.stdout.write('Moving %s' % image_path)
+            image_file = File(image_path)
+            entry.image_file.save(image_path, image_file, True)
+            #os.remove(image_path)
 
-        except Exception, e:
-            raise CommandError(e.message)
+        # except Exception, e:
+        #     raise CommandError(e.message)
 
         self.stdout.write('Successfully updated all entry images.')
