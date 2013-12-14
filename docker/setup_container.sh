3d1fff1650f355d2afbfefaff49c09e1857db762
Tim Sutton <tim@linfiniti.com>
2013-12-14 23:54:24 +0200

    Further tweaks for docker setup (still in progress)

--- docker/setup_container.sh
+++ docker/setup_container.sh
@@ -6,4 +6,6 @@
 
 
 sudo docker build -t linfiniti/projecta .
-sudo docker run -p 7222:22 -p 7280:80 -t -d linfiniti/projecta
+# Setup dir for storing docker shared volume for apt-cache
+sudo mkdir -p /var/docker/volumes/apt-cache/
+sudo docker run -p 7222:22 -p 7280:80 -v /var/docker/volumes/apt-cache/:/var/cache -t -d linfiniti/projecta
