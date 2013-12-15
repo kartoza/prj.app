facb50846324ea929fd98a798df8247eac14b943
Tim Sutton <tim@linfiniti.com>
2013-12-16 00:14:48 +0200

    Docker updates

--- docker/setup_container.sh
+++ docker/setup_container.sh
@@ -4,8 +4,9 @@
 # Because I prefer to set up the container using fabgis. And we want to
 # avoid 42 layer limit.
 
-
+cp ~/.ssh/id_dsa.pub .
 sudo docker build -t linfiniti/projecta .
 # Setup dir for storing docker shared volume for apt-cache
 sudo mkdir -p /var/docker/volumes/apt-cache/
-sudo docker run -p 7222:22 -p 7280:80 -v /var/docker/volumes/apt-cache/:/var/cache -t -d linfiniti/projecta
+sudo docker run -p 7222:22 -p 7280:80 -v /var/docker/volumes/apt-cache/:/var/cache -t -d linfiniti/projecta supervisord -n
+ssh root@localhost -p 7222 "chown -R wsgi.wsgi /home/web"
