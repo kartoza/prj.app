066a4d1e32d94e903c39cad685e6c2d5a312a08d
George Irwin <george@theqrust.com>
2014-01-15 10:22:45 +0200

    Closes #14

--- django_project/core/urls.py
+++ django_project/core/urls.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 """Project level url handler."""
 from django.conf.urls import patterns, include, url
-
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
 admin.autodiscover()
 
@@ -14,5 +14,12 @@ urlpatterns = patterns(
     url(r'^', include('changes.urls')),
     url(r'^', include('vota.urls')),
     url(r'^', include('github_issue.urls')),
+
+    # This over-ride is required to fix 500 errors as per:
+    # https://github.com/bread-and-pepper/django-userena/issues/380
+    url(r'^password/reset/done/$',
+        auth_views.password_reset_done,
+        {'template_name': 'userena/password_reset_done.html'},
+        name='password_reset_done'),
     url(r'^accounts/', include('userena.urls')),
 )
