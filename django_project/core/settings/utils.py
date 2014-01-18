545b232dbfc6ef77e5e9751ff248745ba3214b1b
Tim Sutton <tim@linfiniti.com>
2014-01-18 16:13:49 +0200

    Small code standards changing only.

--- django_project/core/settings/utils.py
+++ django_project/core/settings/utils.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+"""Helpers for settings."""
 import os
 
 # Absolute filesystem path to the Django project directory:
@@ -8,12 +10,21 @@ DJANGO_ROOT = os.path.dirname(
 
 
 def absolute_path(*args):
+    """Get an absolute path for a file that is relative to the django root.
+
+    :param args: List of path elements.
+    :type args: list
+
+    :returns: An absolute path.
+    :rtype: str
+    """
     return os.path.join(DJANGO_ROOT, *args)
 
 
 def ensure_secret_key_file():
-    """Checks that secret.py exists in settings dir. If not, creates one
-with a random generated SECRET_KEY setting."""
+    """Checks that secret.py exists in settings dir.
+
+    If not, creates one with a random generated SECRET_KEY setting."""
     secret_path = absolute_path('core', 'settings', 'secret.py')
     if not os.path.exists(secret_path):
         from django.utils.crypto import get_random_string
