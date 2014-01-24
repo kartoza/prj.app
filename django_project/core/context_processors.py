c59fa1760dd0783fc086533c427005ec6ca64017
George Irwin <irwingeorge@mac.com>
2014-01-24 10:40:28 +0200

    Added try/except to intercom_id import and removed mail_admins on failure

--- django_project/core/context_processors.py
+++ django_project/core/context_processors.py
@@ -2,19 +2,21 @@
 """
 Our custom context processors
 """
-from django.core.mail import mail_admins
 
 
+# noinspection PyPep8Naming
 def add_intercom_app_id(request):
     """Add our Intercom.io app ID to the context
 
     :param request: Http Request obj
 
     """
-    from core.settings.private import INTERCOM_APP_ID
+    try:
+        from core.settings.private import INTERCOM_APP_ID
+    except ImportError:
+        INTERCOM_APP_ID = None
 
     if INTERCOM_APP_ID:
         return {'intercom_app_id': INTERCOM_APP_ID}
     else:
-        mail_admins('No Intercom App ID!', 'Failed to find Intercom App ID')
         return {}
