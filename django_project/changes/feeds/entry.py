ce433cc058ead3437716625f1e6b0e13664352ad
Ismail Sunni <imajimatika@gmail.com>
2014-04-16 10:14:51 +0700

    Add comments.

--- django_project/changes/feeds/entry.py
+++ django_project/changes/feeds/entry.py
@@ -21,7 +21,22 @@ class EntryFeed(Feed):
     """Feed class for Entry."""
 
     def get_object(self, request, *args, **kwargs):
-        """Return project object that matches the project_slug."""
+        """Return the latest Version object that matches the project_slug.
+
+        :param request: The incoming HTTP request object
+        :type request: Request object
+
+        :param args: Positional arguments
+        :type args: tuple
+
+        :param kwargs: Keyword arguments
+        :type kwargs: dict
+
+        :returns: latest version of a project
+        :rtype: Version
+
+        :raises: Http404
+        """
         try:
             project_slug = kwargs.get('project_slug', None)
             project = Project.objects.get(slug=project_slug)
@@ -32,30 +47,71 @@ class EntryFeed(Feed):
             raise Http404('Sorry! We could not find your project!')
 
     def title(self, obj):
-        """Set the title of the RSS."""
+        """Return a title for the RSS.
+
+        :param obj: Latest version of a project
+        :type obj: Version
+
+        :returns: Title of the RSS Feed.
+        :rtype: str
+        """
         return 'RSS Entry of %s Project\'s latest version' % obj.project.name
 
     def description(self, obj):
-        """Set the description of the RSS."""
+        """Return a description for the RSS.
+
+        :param obj: Latest version of a project
+        :type obj: Version
+
+        :returns: Description of the RSS Feed.
+        :rtype: str
+        """
         return ('These are the entries from the latest version of %s project.'
                 % obj.project.name)
 
     def link(self, obj):
-        """Set the url of the project."""
+        """Return the url of the latest version.
+
+        :param obj: Latest version of a project
+        :type obj: Version
+
+        :returns: Url of the latest version.
+        :rtype: str
+        """
         return obj.get_absolute_url()
 
     def items(self, obj):
         """Return all entries form the latest version of a project.
 
         Only for approved version and entry.
+
+        :param obj: Latest version of a project
+        :type obj: Version
+
+        :returns: List of approved entry from the latest version of a project
+        :rtype: list
         """
         return Entry.objects.filter(version=obj, approved=True).order_by(
             '-datetime_created')
 
     def item_title(self, item):
-        """Return the title of the version."""
+        """Return the title of the entry.
+
+        :param item: Entry object from the latest version of a project
+        :type item: Entry
+
+        :returns: title of the entry
+        :rtype: str
+        """
         return item.title
 
     def item_description(self, item):
-        """Return the description of the version."""
+        """Return the description of the entry.
+
+        :param item: Entry object from the latest Version of a Project
+        :type item: Entry
+
+        :returns: description of the Entry
+        :rtype: str
+        """
         return item.description
--- django_project/changes/feeds/version.py
+++ django_project/changes/feeds/version.py
@@ -19,31 +19,88 @@ class VersionFeed(Feed):
     """Feed class for version."""
 
     def get_object(self, request, *args, **kwargs):
-        """Return project object that matches the project_slug."""
+        """Return project object that matches the project_slug.
+
+        :param request: The incoming HTTP request object
+        :type request: HttpRequest
+
+        :param args: Positional arguments
+        :type args: tuple
+
+        :param kwargs: Keyword arguments
+        :type kwargs: dict
+
+        :returns: A project
+        :rtype: Project
+
+        :raises: Http404
+        """
         project_slug = kwargs.get('project_slug', None)
         return get_object_or_404(Project, slug=project_slug)
 
     def title(self, obj):
-        """Set the title of the RSS."""
+        """Return a title for the RSS.
+
+         :param obj: A project
+         :type obj: Project
+
+         :returns: Title of the RSS Feed.
+         :rtype: str
+         """
         return 'RSS Version of %s Project' % obj.name
 
     def description(self, obj):
-        """Set the description of the RSS."""
+        """Return a description for the RSS.
+
+         :param obj: A project
+         :type obj: Project
+
+         :returns: Description of the RSS Feed.
+         :rtype: str
+         """
         return 'These are the latest version of %s project.' % obj.name
 
     def link(self, obj):
-        """Set the url of the project."""
+        """Return the url of the latest version.
+
+        :param obj: Latest version of a project
+        :type obj: Version
+
+        :returns: Url of the latest version.
+        :rtype: str
+        """
         return obj.get_absolute_url()
 
     def items(self, obj):
-        """Return last 5 (if possible) version of the project."""
+        """Return last 5 (if possible) version of the project.
+
+        :param obj: A project
+        :type obj: Project
+
+        :returns: List of approved version of a project
+        :rtype: list
+        """
         return Version.objects.filter(project=obj, approved=True).order_by(
             '-datetime_created')[:5]
 
     def item_title(self, item):
-        """Return the title of the version."""
+        """Return the title of the version.
+
+        :param item: Version object of a project
+        :type item: Version
+
+        :returns: title of the Version
+        :rtype: str
+        """
         return item.name
 
     def item_description(self, item):
-        """Return the description of the version."""
+        """Return the description of the version.
+
+        :param item: Version object of a project
+        :type item: Version
+
+        :returns: description of the Version
+        :rtype: str
+        """
         return item.description
