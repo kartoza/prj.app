2a717af7476fb79a15f5a8e7fc2bf2bf5069ffb4
Ismail Sunni <imajimatika@gmail.com>
2014-04-23 13:21:13 +0700

    Add version_slugify method.

---
+++ django_project/common/__init__.py
@@ -0,0 +1,6 @@
+__author__ = 'ismailsunni'
+__project_name = 'projecta'
+__filename = '__init__.py'
+__date__ = '4/23/14'
+__copyright__ = 'imajimatika@gmail.com'
+__doc__ = ''
---
+++ django_project/common/utilities.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+"""**Utilities functions**
+"""
+
+__author__ = 'Ismail Sunni <ismail@linfiniti.com>'
+__revision__ = '$Format:%H$'
+__date__ = '23/04/2014'
+__license__ = ''
+__copyright__ = ''
+
+
+from slugify import Slugify
+
+version_slugify = Slugify()
+version_slugify.safe_chars = '.'
