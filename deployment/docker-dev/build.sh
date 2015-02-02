64c2029199eba20c0cb8f48e53256883a4500769
Tim Sutton <tim@linfiniti.com>
2015-02-02 13:18:20 +0200

    Fix project specific references

---
+++ deployment/docker-dev/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t aifdr/inasafe-django-dev .
--- django_project/core/settings/prod_docker.py
+++ django_project/core/settings/prod_docker.py
@@ -1,3 +1,4 @@
+# noinspection PyUnresolvedReferences
 from .prod import *  # noqa
 import os
 print os.environ
@@ -5,11 +6,14 @@ print os.environ
 ALLOWED_HOSTS = ['*']
 
 ADMINS = (
-    ('Tim Sutton', 'tim@kartoza.com'),)
+    ('Tim Sutton', 'tim@kartoza.com'),
+    ('Ismail Sunni', 'ismail@kartoza.com'),
+    ('Christian Christellis', 'christian@kartoza.com'),
+    ('Akbar Gumbira', 'akbargumbira@gmail.com'),)
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': os.environ['DATABASE_NAME'],
         'USER': os.environ['DATABASE_USERNAME'],
         'PASSWORD': os.environ['DATABASE_PASSWORD'],
@@ -18,3 +22,20 @@ DATABASES = {
         'TEST_NAME': 'unittests',
     }
 }
+
+MEDIA_ROOT = '/home/web/media'
+STATIC_ROOT = '/home/web/static'
+
+# See fig.yml file for postfix container definition
+#
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# Host for sending e-mail.
+EMAIL_HOST = 'smtp'
+# Port for sending e-mail.
+EMAIL_PORT = 25
+# SMTP authentication information for EMAIL_HOST.
+# See fig.yml for where these are defined
+EMAIL_HOST_USER = 'noreply@kartoza.com'
+EMAIL_HOST_PASSWORD = 'docker'
+EMAIL_USE_TLS = False
+EMAIL_SUBJECT_PREFIX = '[InaSAFE]'
