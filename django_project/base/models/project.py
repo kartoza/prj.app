c6acc66bd738a69c14561041705033a8acb436a3
Tim Sutton <tim@kartoza.com>
2015-09-10 14:54:18 +0200

    Another place where version should be sorted by padded version

--- django_project/base/models/project.py
+++ django_project/base/models/project.py
@@ -118,5 +118,5 @@ class Project(AuditedModel):
 
     def versions(self):
         """Get all the versions for this project."""
-        qs = Version.objects.filter(project=self).order_by('-name')
+        qs = Version.objects.filter(project=self).order_by('-padded_version')
         return qs
