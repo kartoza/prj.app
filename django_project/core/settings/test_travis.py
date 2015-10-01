ab2ceb01edb829fd4aaee501172a908ba6b1157b
Rischan Mafrur <rischanlab@gmail.com>
2015-10-01 10:04:28 +0700

    added .travis.yml for testing

---
+++ django_project/core/settings/test_travis.py
@@ -0,0 +1,16 @@
+__author__ = 'rischan'
+
+# -*- coding: utf-8 -*-
+from .test import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'test_db',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        # Set to empty string for default.
+        'PORT': '',
+    }
+}
