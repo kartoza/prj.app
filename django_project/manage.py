036d9e934c009571fb5ff26bd28c19cf033798dc
Rischan Mafrur <rischanlab@gmail.com>
2015-10-05 09:46:38 +0700

    restore DJANGO_SETTINGS_MODULE to core.settings.dev

--- django_project/manage.py
+++ django_project/manage.py
@@ -9,7 +9,7 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault(
         "DJANGO_SETTINGS_MODULE",
-        "core.settings.dev_rischan")
+        "core.settings.dev")
 
     from django.core.management import execute_from_command_line
 
