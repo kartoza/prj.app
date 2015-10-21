51952490c077ac00c9aa6940a59ac1715c2af034
Tim Sutton <tim@kartoza.com>
2015-10-21 15:14:29 +0200

    Fix #125 - add Rischan to the admin list

--- django_project/core/settings/base.py
+++ django_project/core/settings/base.py
@@ -7,7 +7,8 @@ core.settings.base
 from .utils import absolute_path
 
 ADMINS = (
-    ('Tim Sutton', 'tim@linfiniti.com'),
+    ('Tim Sutton', 'tim@kartoza.com'),
+    ('Rischan Mafrur', 'rischan@kartoza.com')
 )
 SERVER_EMAIL = 'tim@linfiniti.com'
 EMAIL_HOST = 'localhost'
@@ -19,7 +20,7 @@ MANAGERS = ADMINS
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Africa/Johannesburg'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
