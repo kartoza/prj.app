4a7e5eb45577d2788eedb91190f564c0ecf5c7b7
Rischan Mafrur <rischanlab@gmail.com>
2015-11-04 05:59:51 +0700

    fixing sql for sponsor

--- deployment/sql/sponsor.sql
+++ deployment/sql/sponsor.sql
@@ -1,13 +1,4 @@
 
-CREATE INDEX changes_sponsor_creator_id ON changes_sponsor USING btree (creator_id);
-CREATE INDEX changes_sponsor_editor_id ON changes_sponsor USING btree (editor_id);
-CREATE INDEX changes_sponsor_name_like ON changes_sponsor USING btree (name varchar_pattern_ops);
-CREATE INDEX changes_sponsor_project_id ON changes_sponsor USING btree (project_id);
-CREATE INDEX changes_sponsor_slug ON changes_sponsor USING btree (slug);
-CREATE INDEX changes_sponsor_slug_like ON changes_sponsor USING btree (slug varchar_pattern_ops);
-CREATE INDEX changes_sponsor_author_id ON changes_sponsor USING btree (author_id);
-
-
 CREATE TABLE changes_sponsor (
     id integer NOT NULL,
     datetime_created timestamp with time zone NOT NULL,
@@ -28,11 +19,16 @@ CREATE TABLE changes_sponsor (
     image_file character varying(100) NOT NULL,
     project_id integer NOT NULL,
     slug character varying(50),
-    author_id integer NOT NULL,
+    author_id integer NOT NULL
 );
 
-
-
+CREATE INDEX changes_sponsor_creator_id ON changes_sponsor USING btree (creator_id);
+CREATE INDEX changes_sponsor_editor_id ON changes_sponsor USING btree (editor_id);
+CREATE INDEX changes_sponsor_name_like ON changes_sponsor USING btree (name varchar_pattern_ops);
+CREATE INDEX changes_sponsor_project_id ON changes_sponsor USING btree (project_id);
+CREATE INDEX changes_sponsor_slug ON changes_sponsor USING btree (slug);
+CREATE INDEX changes_sponsor_slug_like ON changes_sponsor USING btree (slug varchar_pattern_ops);
+CREATE INDEX changes_sponsor_author_id ON changes_sponsor USING btree (author_id);
 
 ALTER TABLE ONLY changes_sponsor
 ADD CONSTRAINT changes_sponsor_pkey PRIMARY KEY (id);
