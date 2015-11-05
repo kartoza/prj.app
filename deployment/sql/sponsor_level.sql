9a1969ce24fb2cccb46bd152c168b8c68c411a5e
Rischan Mafrur <rischanlab@gmail.com>
2015-11-05 20:01:14 +0700

    added sponsor level query to sql file |  this query for creating sponsor level table

--- deployment/sql/sponsor_level.sql
+++ deployment/sql/sponsor_level.sql
@@ -1,8 +1,11 @@
 CREATE TABLE changes_sponsorlevel (
     id integer NOT NULL,
+    datetime_created timestamp with time zone NOT NULL,
+    datetime_modified timestamp with time zone NOT NULL,
+    creator_id integer,
+    editor_id integer,
     name character varying(255) NOT NULL,
-    logo character varying(255) NOT NULL,
-    author_id integer NOT NULL
+    logo character varying(255) NOT NULL
 );
 
 CREATE SEQUENCE changes_sponsorlevel_id_seq
@@ -13,3 +16,7 @@ CREATE SEQUENCE changes_sponsorlevel_id_seq
     CACHE 1;
 
 ALTER TABLE changes_sponsorlevel ALTER id SET DEFAULT NEXTVAL('changes_sponsorlevel_id_seq');
+
+ALTER TABLE ONLY changes_sponsorlevel
+ADD CONSTRAINT changes_sponsorlevel_pkey PRIMARY KEY (id);
+
