a30ce798245c44318d7fdd589ba2443ad0d19395
Rischan Mafrur <rischanlab@gmail.com>
2015-12-02 13:57:19 +0700

    PEP8

--- django_project/base/views/error_views.py
+++ django_project/base/views/error_views.py
@@ -22,8 +22,10 @@ def custom_404(request, template_name='404.html'):
     """
     public_projects = Project.objects.filter(approved=True, private=False)
 
-    response = render_to_response(template_name, {
-        'request_path': request.path,
-        'projects': public_projects},context_instance=RequestContext(request))
+    response = render_to_response(
+        template_name, {
+            'request_path': request.path,
+            'projects': public_projects},
+        context_instance=RequestContext(request))
     response.status_code = 404
     return response
