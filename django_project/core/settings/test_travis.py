dd00ab972d25ae1cde8cb4c0c91b49e8c9028405
Rischan Mafrur <rischanlab@gmail.com>
2016-01-16 10:54:52 +0700

    Using pipelinestorage for travis test

--- django_project/core/settings/test_travis.py
+++ django_project/core/settings/test_travis.py
@@ -4,6 +4,8 @@ __author__ = 'rischan'
 # -*- coding: utf-8 -*-
 from .test import *  # noqa
 
+STATICFILES_STORAGE = 'pipeline.storage.PipelineStorage'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
