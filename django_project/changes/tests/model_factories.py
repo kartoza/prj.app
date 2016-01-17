dd2108184b5a9153c42eae0a4ad92801d88bba71
Rischan Mafrur <rischanlab@gmail.com>
2016-01-17 23:29:09 +0700

    factories for creating model instance sponsors, sponsorship level and period

--- django_project/changes/tests/model_factories.py
+++ django_project/changes/tests/model_factories.py
@@ -2,9 +2,14 @@
 """Factories for creating model instances for testing."""
 
 import factory
+import factory.fuzzy
+import datetime
 from changes.models.category import Category
 from changes.models.entry import Entry
 from changes.models.version import Version
+from changes.models.sponsorship_level import SponsorshipLevel
+from changes.models.sponsor import Sponsor
+from changes.models.sponsorship_period import SponsorshipPeriod
 from core.model_factories import UserF
 
 
@@ -51,3 +56,53 @@ class VersionF(factory.django.DjangoModelFactory):
     author = factory.SubFactory(UserF)
     description = u'This description is really only here for testing'
     project = factory.SubFactory('base.tests.model_factories.ProjectF')
+
+
+class SponsorF(factory.django.DjangoModelFactory):
+    """
+    Sponsors model factory
+    """
+    class Meta:
+        model = Sponsor
+
+    name = factory.Sequence(lambda n: u'Test Sponsor Name %s' % n)
+    sponsor_url = factory.Sequence(lambda n: u'Test URL %s' % n)
+    contact_person = factory.Sequence(lambda n: u'Test Contact Person %s' % n)
+    sponsor_email = factory.Sequence(lambda n: u'Test Sponsor Email %s' % n)
+    approved = True
+    author = factory.SubFactory(UserF)
+    logo = factory.django.ImageField(color='green')
+    agreement = factory.django.ImageField(color='green')
+    project = factory.SubFactory('base.tests.model_factories.ProjectF')
+
+
+class SponsorshipLevelF(factory.django.DjangoModelFactory):
+    """
+    Sponsorship Level model factory
+    """
+    class Meta:
+        model = SponsorshipLevel
+
+    name = factory.Sequence(lambda n: u'Test Sponsorship level %s' % n)
+    currency = factory.Sequence(lambda n: u'Test Currency %s' % n)
+    approved = True
+    author = factory.SubFactory(UserF)
+    value = factory.Sequence(lambda n: n)
+    logo = factory.django.ImageField(color='green')
+    project = factory.SubFactory('base.tests.model_factories.ProjectF')
+
+
+class SponsorshipPeriodF(factory.django.DjangoModelFactory):
+    """
+    Sponsorship Period model factory
+    """
+    class Meta:
+        model = SponsorshipPeriod
+
+    start_date = factory.fuzzy.FuzzyDate(datetime.date(2014, 1, 1))
+    end_date = factory.fuzzy.FuzzyDate(datetime.date(2015, 1, 1))
+    approved = True
+    author = factory.SubFactory(UserF)
+    project = factory.SubFactory('base.tests.model_factories.ProjectF')
+    sponsor_id = factory.Sequence(lambda n: n)
+    sponsorshiplevel_id = factory.Sequence(lambda n: n)
