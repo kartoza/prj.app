545d94a44a70fe77b8c300bcba42bd65654d4518
ann26 <anita.hapsari15@alumni.imperial.ac.uk>
2017-03-09 10:14:16 +0700

    add unicode to handle special characters

--- django_project/base/models/project.py
+++ django_project/base/models/project.py
@@ -129,7 +129,7 @@ class Project(models.Model):
         if not self.pk:
             words = self.name.split()
             filtered_words = [t for t in words if t.lower() not in STOP_WORDS]
-            #unidecode() takes Unicode data (e.g. special characters) and represents it in ASCII characters
+            # unidecode() represents special characters (unicode data) in ASCII
             new_list = unidecode(' '.join(filtered_words))
             self.slug = slugify(new_list)[:50]
 
