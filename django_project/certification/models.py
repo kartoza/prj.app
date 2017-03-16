702854ce8a9f47a6edc1f09f880916c30f421b87
ann26 <anita.hapsari15@alumni.imperial.ac.uk>
2017-03-16 10:21:44 +0700

    add certification app (#392)

--- django_project/certification/models.py
+++ django_project/certification/models.py
@@ -1,10 +1,13 @@
+# coding=utf-8
+"""
+Model definitions for certification apps
+"""
 
 import os
 import logging
 import string
 import re
 from django.core.urlresolvers import reverse
-# from __future__ import unicode_literals
 from django.db import models
 from django.utils.text import slugify
 from django.conf.global_settings import MEDIA_ROOT
@@ -16,29 +19,66 @@ from unidecode import unidecode
 
 
 logger = logging.getLogger(__name__)
-
-class ApprovedTrainingCenterManager(models.Manager):
-    """ Custom training centre manager that shows only approved conveners """
-
-    def get_queryset(self):
-        """ Query set generator """
-        return super(
-            ApprovedTrainingCenterManager, self).get_queryset().filter(approved=True)
+app_label = 'certification'
 
 
-class UnapprovedTrainingCenterManager(models.Manager):
-    """ Custom training centre manager that shows only unapproved training centre """
+class SlugModel(object):
+    
+    slug = models.SlugField(unique=True)
 
-    def get_queryset(self):
-        """ Query set generator """
-        return super(
-            UnapprovedTrainingCenterManager, self).get_queryset().filter(approved=False)
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            words = self.name.split()
+            filtered_words = [t for t in words if t.lower() not in STOP_WORDS]
+            # unidecode() represents special characters (unicode data) in ASCII
+            new_list = unidecode(' '.join(filtered_words))
+            self.slug = slugify(new_list)[:50]
+
+        super(SlugModel, self).save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
 
 
-class TrainingCenter(models.Model):
-    """ Training Centre / Organisation registration """
+class CourseType(SlugModel, models.Model):
+    """Course Type model."""
+
     name = models.CharField(
-        help_text=_('Organisation/Institution name who intend to be a Training Center'),
+        help_text="Course type.",
+        max_length=200,
+        null=False,
+        blank=False
+    )
+
+    slug = models.SlugField(unique=True)
+    objects = models.Manager()
+    course = models.ManyToManyField(Course)
+
+    # noinspection PyClassicStyleClass.
+    class Meta:
+        """Meta class for Course types."""
+
+        ordering = ['name']
+
+    def save(self, *args, **kwargs):
+        super(CourseType, self).save(self, *args, **kwargs)
+
+    def __unicode__(self):
+        return u'%s' % self.name
+
+    def get_absolute_url(self):
+        """Return URL to course type detail page.
+        :return: URL
+        :rtype: str
+        """
+        return reverse('course-type-detail', kwargs={'slug': self.slug})
+
+
+class TrainingCenter(SlugModel, models.Model):
+    """Training Centre / Organisation registration."""
+
+    name = models.CharField(
+        help_text=_('Organisation/Institution name.'),
         max_length=150,
         null=False,
         blank=False,
@@ -46,21 +86,21 @@ class TrainingCenter(models.Model):
     )
 
     email = models.CharField(
-        help_text=_('Valid email address for communication purpose'),
+        help_text=_('Valid email address for communication purpose.'),
         max_length=150,
         null=False,
         blank=False
     )
 
     Address = models.CharField(
-        help_text=_('Address of the organisation/institution'),
+        help_text=_('Address of the organisation/institution.'),
         max_length=250,
         null=False,
         blank=False,
     )
 
     phone = models.CharField(
-        help_text=_('Phone number/Landline'),
+        help_text=_('Phone number/Landline.'),
         max_length=150,
         null=False,
         blank=False
@@ -68,155 +108,47 @@ class TrainingCenter(models.Model):
 
     slug = models.SlugField(unique=True)
     objects = models.Manager()
-    approved_objects = ApprovedTrainingCenterManager()
-    unapproved_objects = UnapprovedTrainingCenterManager()
+    course = models.ManyToManyField(Course)
 
     # noinspection PyClassicStyleClass.
     class Meta:
-        """ Meta class for training centre. """
-        app_label = 'certification'
+        """Meta class for training centre."""
+
         ordering = ['name']
 
     def save(self, *args, **kwargs):
-        """ Overloaded save method
-        :param args:
-        :param kwargs:
-        """
-        if not self.pk:
-            words = self.name.split()
-            filtered_words = [t for t in words if t.lower() not in STOP_WORDS]
-            # unidecode() represents special characters (unicode data) in ASCII
-            new_list = unidecode(' '.join(filtered_words))
-            self.slug = slugify(new_list)[:50]
-
-        super(TrainingCenter, self).save(*args, **kwargs)
+        super(TrainingCenter, self).save(self, *args, **kwargs)
 
     def __unicode__(self):
         return u'%s' % self.name
 
     def get_absolute_url(self):
-        """Return URL to training center detail page
+        """Return URL to training center detail page.
         :return: URL
         :rtype: str
         """
         return reverse('training-center-detail', kwargs={'slug': self.slug})
 
 
-class CourseType(models.Model):
-    """ Course Type model """
-    name = models.CharField(
-        help_text="Course type",
+class Attendee(SlugModel, models.Model):
+    """Course Attendee model."""
+
+    firstname = models.CharField(
+        help_text="First name course attendee.",
         max_length=200,
         null=False,
         blank=False
     )
 
-    slug = models.SlugField(unique=True)
-    objects = models.Manager()
-    training_center = models.ManyToManyField(
-        TrainingCenter,
-        through='Membership',
-        through_fields=('coursetype', 'trainingcenter')
-    )
-
-    # noinspection PyClassicStyleClass.
-    class Meta:
-        """ Meta class for Course types """
-        app_label = 'certification'
-        ordering = ['name']
-
-    def save(self, *args, **kwargs):
-        """ Overloaded save method
-        :param args:
-        :param kwargs:
-        """
-        if not self.pk:
-            words = self.name.split()
-            filtered_words = [t for t in words if t.lower() not in STOP_WORDS]
-            # unidecode() represents special characters (unicode data) in ASCII
-            new_list = unidecode(' '.join(filtered_words))
-            self.slug = slugify(new_list)[:50]
-
-        super(CourseType, self).save(*args, **kwargs)
-
-    def __unicode__(self):
-        return u'%s' % self.name
-
-
-class Membership(models.Model):
-    """ Many to many relationship for Course Type and Training Center """
-    coursetype = models.ForeignKey(CourseType, on_delete=models.CASCADE)
-    trainingcenter = models.ForeignKey(TrainingCenter, on_delete=models.CASCADE)
-
-
-class CourseConvener(models.Model):
-    """ Register Course Convener """
-    name = models.CharField(
-        help_text="First name of course conveners",
-        max_length=150,
-        null=False,
-        blank=False
-    )
-
-    surename = models.CharField(
-        help_text="Surename course conveners",
-        max_length=150,
-        null=False,
-        blank=False
-    )
-
-    email = models.CharField(
-        help_text="Email address",
-        max_length=200,
-        null=False,
-        blank=False
-    )
-
-    slug = models.SlugField(unique=True)
-    objects = models.Manager()
-
-    # noinspection PyClassicStyleClass.
-    class Meta:
-        """ Meta class for Course convener """
-        app_label = 'certification'
-        ordering = ['name']
-
-    def save(self, *args, **kwargs):
-        """ Overloaded save method
-        :param args:
-        :param kwargs:
-        """
-        if not self.pk:
-            words = self.name.split()
-            filtered_words = [t for t in words if t.lower() not in STOP_WORDS]
-            # unidecode() represents special characters (unicode data) in ASCII
-            new_list = unidecode(' '.join(filtered_words))
-            self.slug = slugify(new_list)[:50]
-
-        super(CourseConvener, self).save(*args, **kwargs)
-
-    def __unicode__(self):
-        return u'%s' % self.name
-
-
-class CourseAttendee(models.Model):
-    """ Course Attendee model """
-    name = models.CharField(
-        help_text="First name course attendee",
-        max_length=200,
-        null=False,
-        blank=False
-    )
-
-    surename = models.CharField(
-        help_text="Surename course attendee",
+    surname = models.CharField(
+        help_text="Surname course attendee.",
         max_length=200,
         null=False,
         blank=False
     )
 
     email = models.CharField(
-        help_text="Email address",
+        help_text="Email address.",
         max_length=200,
         null=False,
         blank=False
@@ -224,79 +156,168 @@ class CourseAttendee(models.Model):
 
     slug = models.SlugField(unique=True)
     objects = models.Manager()
+    certificate = models.ForeignKey(Certificate, models.CASCADE)
 
     # noinspection PyClassicStyleClass.
     class Meta:
-        """ Meta class for Course attendee """
-        app_label = 'certification'
-        ordering = ['name']
+        """ Meta class for Course attendee."""
+
+        ordering = ['firstname']
 
     def save(self, *args, **kwargs):
-        """ Overloaded save method
-        :param args:
-        :param kwargs:
-        """
-        if not self.pk:
-            words = self.name.split()
-            filtered_words = [t for t in words if t.lower() not in STOP_WORDS]
-            # unidecode() represents special characters (unicode data) in ASCII
-            new_list = unidecode(' '.join(filtered_words))
-            self.slug = slugify(new_list)[:50]
-
-        super(CourseAttendee, self).save(*args, **kwargs)
+        super(Attendee, self).save(self, *args, **kwargs)
 
     def __unicode__(self):
         return u'%s' % self.name
 
+    def get_absolute_url(self):
+        """Return URL to attendee detail page.
+        :return: URL
+        :rtype: str
+        """
+        return reverse('attendee-detail', kwargs={'slug': self.slug})
 
-class CertifyingOrganisation(models.Model):
+
+class Certificate(models.Model):
+    """Certificate model."""
+
+    def get_absolute_url(self):
+        """Return URL to certificate detail page.
+        :return: URL
+        :rtype: str
+        """
+        return reverse('certificate-detail', kwargs={'slug': self.slug})
+
+
+class Course(SlugModel, models.Model):
+    """Course model."""
+
     name = models.CharField(
-        help_text="Name of Organisation or Institution",
+        help_text="Course name.",
+        max_length=200,
+        null=False,
+        blank=False,
+    )
+
+    def save(self, *args, **kwargs):
+        super(Course, self).save(self, *args, **kwargs)
+
+    slug = models.SlugField(unique=True)
+    objects = models.Manager()
+    course_attendee = models.ManyToManyField(Attendee)
+    certificate = models.ForeignKey(Certificate, models.CASCADE)
+
+    def __unicode__(self):
+        return u'%s' %self.name
+
+    def get_absolute_url(self):
+        """Return URL to course detail page.
+        :return: URL
+        :rtype: str
+        """
+        return reverse('course-detail', kwargs={'slug': self.slug})
+
+
+class CourseConvener(SlugModel, models.Model):
+    """Course Convener model."""
+
+    slug = models.SlugField(unique=True)
+    objects = models.Manager()
+    course = models.ManyToManyField(Course)
+
+    def save(self, *args, **kwargs):
+        super(CourseConvener, self).save(self, *args, **kwargs)
+
+    def __unicode__(self):
+        return u'%s' % self.name
+
+    def get_absolute_url(self):
+        """Return URL to course convener detail page.
+        :return: URL
+        :rtype: str
+        """
+        return reverse('course-convener-detail', kwargs={'slug': self.slug})
+
+
+class ApprovedCertifyingOrganisationManager(models.Manager):
+    """Custom training centre manager, shows only approved training center."""
+
+    def get_queryset(self):
+        """ Query set generator. """
+
+        return super(
+            ApprovedCertifyingOrganisationManager, self).get_queryset().filter(
+                approved=True)
+
+
+class UnapprovedCertifyingOrganisationManager(models.Manager):
+    """Custom training centre manager, shows only unapproved training centre."""
+
+    def get_queryset(self):
+        """ Query set generator. """
+
+        return super(
+            UnapprovedCertifyingOrganisationManager, self).get_queryset(
+        ).filter(approved=False)
+
+
+class CertifyingOrganisation(SlugModel, models.Model):
+    """ Certifying organisation model."""
+
+    organisation_name = models.CharField(
+        help_text="Name of Organisation or Institution.",
         max_length=200,
         null=False,
         blank=False
     )
 
     organisation_email = models.CharField(
-        help_text="Email address Organisation or Institution",
+        help_text="Email address Organisation or Institution.",
         max_length=200,
         null=False,
         blank=False
     )
 
     organisation_phone = models.CharField(
-        help_text="Contact of Organisation or Institution",
+        help_text="Contact of Organisation or Institution.",
         max_length=200,
         null=False,
         blank=False
     )
 
+    approved = models.BooleanField(
+        help_text="Approval from project admin",
+        default=False
+    )
+
     slug = models.SlugField(unique=True)
+
+    course_type = models.ManyToManyField(CourseType)
+    course = models.ManyToManyField(Course)
+    training_center = models.ManyToManyField(TrainingCenter)
+    course_convener = models.ManyToManyField(CourseConvener)
+    project = models.ForeignKey('base.Project')
     objects = models.Manager()
+    approved_objects = ApprovedCertifyingOrganisationManager()
+    unapproved_objects = UnapprovedCertifyingOrganisationManager()
 
     # noinspection PyClassicStyleClass.
     class Meta:
-        """ Meta class for Course attendee """
-        app_label = 'certification'
-        ordering = ['name']
+        """ Meta class for Course attendee."""
+
+        ordering = ['organisation_name']
 
     def save(self, *args, **kwargs):
-        """ Overloaded save method
-        :param args:
-        :param kwargs:
-        """
-        if not self.pk:
-            words = self.name.split()
-            filtered_words = [t for t in words if t.lower() not in STOP_WORDS]
-            # unidecode() represents special characters (unicode data) in ASCII
-            new_list = unidecode(' '.join(filtered_words))
-            self.slug = slugify(new_list)[:50]
-
-        super(CertifyingOrganisation, self).save(*args, **kwargs)
+        super(CertifyingOrganisation, self).save(self, *args, **kwargs)
 
     def __unicode__(self):
         return u'%s' % self.name
 
+    def get_absolute_url(self):
+        """Return URL to certifying organisation detail page.
+        :return: URL
+        :rtype: str
+        """
+        return reverse(
+            'certifying-organisation-detail', kwargs={'slug': self.slug})
 
-class Certificate(models.Model):
-    student = models.ForeignKey(CourseAttendee, on_delete=models.CASCADE)
