40cf9bd56e7d9178f08ac897baeef81a5c9cbf39
ann26 <anita.hapsari15@alumni.imperial.ac.uk>
2017-03-16 12:05:44 +0700

    add certification app (#392)

---
+++ django_project/certification/tests/tests.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+"""Test for models"""
+
+from django.test import TestCase
+from certification.models import (
+    Certificate,
+    Attendee,
+    Course,
+    CourseType,
+    CourseConvener,
+    CertifyingOrganisation,
+    TrainingCenter)
+
