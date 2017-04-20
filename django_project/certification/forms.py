27b3af208c7762024fb900aa573f8a36018004bb
ann26 <anita.hapsari15@alumni.imperial.ac.uk>
2017-04-20 11:33:54 +0700

    add buttons

--- django_project/certification/forms.py
+++ django_project/certification/forms.py
@@ -59,33 +59,3 @@ class CertifyingOrganisationForm(forms.ModelForm):
         instance.save()
         self.save_m2m()
         return instance
-
-
-class CustomOSMWidget(BaseGeometryWidget):
-    """An OpenLayers/OpenStreetMap-based widget."""
-    template_name = 'gis/openlayers-osm.html'
-    default_lon = 20
-    default_lat = -29
-
-    class Media:
-        js = (
-            '/static/js/libs/OpenLayers-2.13.1/OpenLayers.js',
-            '/static/js/libs/OpenLayers-2.13.1/OpenStreetMapSSL.js',
-            '/static/js/libs/OLMapWidget.js'
-        )
-
-    def __init__(self, attrs=None):
-        super(CustomOSMWidget, self).__init__()
-        for key in ('default_lon', 'default_lat'):
-            self.attrs[key] = getattr(self, key)
-        if attrs:
-            self.attrs.update(attrs)
-
-    @property
-    def map_srid(self):
-        # Use the official spherical mercator projection SRID when GDAL is
-        # available; otherwise, fallback to 900913.
-        if gdal.HAS_GDAL:
-            return 3857
-        else:
-            return 900913
