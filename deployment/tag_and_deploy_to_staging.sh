28f4eaa8ddb1f2f0550e840b23edaf916f385b6f
Tim Sutton <tim@kartoza.com>
2017-08-17 13:04:12 +0200

    Updates to deployment scripts

---
+++ deployment/tag_and_deploy_to_staging.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Use this to tag a version on your local"
+echo "machine, push the tag upstream and then"
+echo "deploy it to the remote staging server."
+echo "e.g."
+echo "$0 version-1.2.3"
+VERSION=$1
+read -p "Are you sure you want to continue? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	VERSION=$1
+    # Get the latest backups and media from production
+	make dbbackup
+	make mediasync
+    # Sync the latest backups and media to staging from production
+    rsync -L  backups/latest.dmp staging.changelog.qgis.org:/home/projecta/deployment/backups/
+    rsync -r -v media/ staging.changelog.qgis.org:/home/projecta/deployment/media
+    # Tag the release and push to main repo
+	git tag $VERSION
+	git push --tags upstream 
+    # Check it out on the server
+    # No migrations are run - you should do that manually for now
+	ssh staging.changelog.qgis.org "cd /home/projecta/deployment && git fetch --tags && git checkout $VERSION && make collectstatic && make reload"
+else
+	echo "Tag and deploy to staging aborted.
+fi
