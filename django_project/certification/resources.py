84df588b269cfee0088451a23aeee8c7f736ce4e
sonlinux <alison.mukoma@yahoo.com>
2017-10-13 16:44:12 +0200

    * conforming module to Travis standard
    Fix #557

--- django_project/certification/resources.py
+++ django_project/certification/resources.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from import_export import resources
 from .models.attendee import Attendee
 
+
 class AttendeeModelResource(resources.ModelResource):
     """Resource for admin csv import and export."""
     class Meta:
