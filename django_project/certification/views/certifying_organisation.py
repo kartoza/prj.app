24a47af37dff4684a910ab87c1df4a0026fc8602
Anita Hapsari <anita@kartoza.com>
2017-10-26 02:20:12 +0700

    add about page (#615)
    * add about page
    
    * add about icon
    
    * clean code
    
    * add about icon
    
    * add info text

--- django_project/certification/views/certifying_organisation.py
+++ django_project/certification/views/certifying_organisation.py
@@ -12,7 +12,8 @@ from django.views.generic import (
     DeleteView,
     DetailView,
     UpdateView,
-    RedirectView)
+    RedirectView,
+    TemplateView)
 from django.http import HttpResponseRedirect, Http404
 from django.db import IntegrityError
 from django.core.exceptions import ValidationError
@@ -409,6 +410,7 @@ class CertifyingOrganisationCreateView(
             CertifyingOrganisationCreateView, self).get_context_data(**kwargs)
         context['certifyingorganisations'] = \
             self.get_queryset().filter(project=self.project)
+        context['the_project'] = self.project
         return context
 
     def form_valid(self, form):
@@ -544,6 +546,7 @@ class CertifyingOrganisationUpdateView(
             CertifyingOrganisationUpdateView, self).get_context_data(**kwargs)
         context['certifyingorganisations'] = self.get_queryset() \
             .filter(project=self.project)
+        context['the_project'] = self.project
         return context
 
     def get_queryset(self):
@@ -728,3 +731,22 @@ class ApproveCertifyingOrganisationView(
         return reverse(self.pattern_name, kwargs={
             'project_slug': project_slug
         })
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+    def get_context_data(self, **kwargs):
+        """Get the context data which is passed to a template.
+
+        :param kwargs: Any arguments to pass to the superclass.
+        :type kwargs: dict
+
+        :returns: Context data which will be passed to the template.
+        :rtype: dict
+        """
+
+        context = super(AboutView, self).get_context_data(**kwargs)
+        project_slug = self.kwargs.get('project_slug')
+        context['the_project'] = Project.objects.get(slug=project_slug)
+        return context
