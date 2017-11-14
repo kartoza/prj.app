361016ecbad93e08ce368feab2e298da11b96d89
sonlinux <alison.mukoma@yahoo.com>
2017-11-14 08:56:48 +0200

    fixing migrations

---
+++ django_project/certification/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('certification', '0006_auto_20170615_0807'),
+        ('certification', '0007_auto_20170622_1119'),
+    ]
+
+    operations = [
+    ]
