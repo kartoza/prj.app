21a04172c15b4ad7bef8582b9e1ed5caa960a012
sonlinux <alison.mukoma@yahoo.com>
2017-12-20 11:02:48 -0500

    add user permission template tag

---
+++ django_project/base/templatetags/user_perm.py
@@ -0,0 +1,20 @@
+from django import template
+from django.contrib.auth.models import User
+
+register = template.Library()
+
+@register.assignment_tag(takes_context=True)
+def get_user_perm(context, perm):
+    try:
+        request = context['request']
+        obj = User.objects.get(user=request.user)
+        obj_perms = obj.permission_tags.all()
+        flag = False
+        for p in obj_perms:
+            if perm.lower() == p.codename.lower():
+                flag = True
+                return flag
+        return flag
+
+    except Exception as e:
+        return ""
