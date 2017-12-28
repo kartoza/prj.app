6aac387644dc5ca5607897244214cc7c40650015
sonlinux <alison.mukoma@yahoo.com>
2017-12-28 11:58:58 -0500

    fix travis tests

--- django_project/base/templatetags/user_perm.py
+++ django_project/base/templatetags/user_perm.py
@@ -1,12 +1,15 @@
 from django import template
 from django.contrib.auth.models import User
 
+
 register = template.Library()
+
 
 @register.assignment_tag(takes_context=True)
 def get_user_perm(context, perm):
     try:
         request = context['request']
+        # obj = Project.objects.get(user=request.user)
         obj = User.objects.get(user=request.user)
         obj_perms = obj.permission_tags.all()
         flag = False
@@ -15,6 +18,5 @@ def get_user_perm(context, perm):
                 flag = True
                 return flag
         return flag
-
-    except Exception as e:
+    except:
         return ""
