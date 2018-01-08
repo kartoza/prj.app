a1616bbe0424b02a70ed15e60093519782e0c872
sonlinux <alison.mukoma@yahoo.com>
2018-01-08 13:36:18 -0500

    fix travis

--- django_project/changes/utils/render_to_pdf.py
+++ django_project/changes/utils/render_to_pdf.py
@@ -6,20 +6,21 @@ from django.conf import settings
 from xhtml2pdf import pisa
 
 
-links = lambda uri, rel: \
-    os.path.join(
-        settings.MEDIA_ROOT,
-    uri.replace(settings.MEDIA_URL, '')
-    )
+def links(uri, rel):
+    link = os.path.join(
+        settings.MEDIA_ROOT, uri.replace(settings.MEDIA_URL, ''))
+    return link
 
 
 def render_to_pdf(template_src, context_dict={}):
+    """Generate PDF from parsed template."""
     template = get_template(template_src)
     html = template.render(context_dict)
     result = BytesIO()
-    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result, link_callback=links)
-    # pdf = pisa.pisaDocument(BytesIO(html.encode('utf-8')), result, link_callback=link_callback)
+    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")),
+                            result, link_callback=links)
 
     if not pdf.err:
-        return HttpResponse(result.getvalue(), content_type="application/pdf")
+        return HttpResponse(result.getvalue(),
+                            content_type="application/pdf")
     return None
