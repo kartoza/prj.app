c1a6bad9bb076071a90276cd6d87cec9339f95df
sonlinux <alison.mukoma@yahoo.com>
2018-02-21 14:45:20 +0200

    conform to standard practice

--- django_project/base/models/project.py
+++ django_project/base/models/project.py
@@ -159,8 +159,9 @@ class Project(models.Model):
     )
 
     project_repository_url = models.URLField(
-        help_text=_('A repository URL for this project. '
-                    'For instance a path to the project\'s GitHub repository.'),
+        help_text=_(
+            'A repository URL for this project. '
+            'For instance a path to the project\'s GitHub repository.'),
         blank=True,
         null=True
     )
