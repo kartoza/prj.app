35d8cb4e3bd4f20560e64e599139c1376dd86d0c
sonlinux <alison.mukoma@yahoo.com>
2018-02-23 11:54:10 +0200

    readd some removed hint message

--- django_project/base/models/project.py
+++ django_project/base/models/project.py
@@ -104,8 +104,9 @@ class Project(models.Model):
 
     image_file = models.ImageField(
         help_text=_('A logo image for this project. '
-                    'The ideal size for your image '
-                    'should be 512 x 512 pixels.'),
+                    'Most browsers support dragging the image '
+                    'directly on to the "Choose File" button above. '
+                    'The ideal size for your image is 512 x 512 pixels.'),
         upload_to=os.path.join(MEDIA_ROOT, 'images/projects'),
         blank=True
     )
