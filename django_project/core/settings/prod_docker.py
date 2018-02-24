fae52c2899ae23e6cdc6e541839306bbaa073df8
Ismail Sunni <imajimatika@gmail.com>
2018-02-24 17:43:47 +0700

    Fix deprecated things in 1.9

--- django_project/core/settings/prod_docker.py
+++ django_project/core/settings/prod_docker.py
@@ -23,7 +23,9 @@ DATABASES = {
         'PASSWORD': os.environ['DATABASE_PASSWORD'],
         'HOST': os.environ['DATABASE_HOST'],
         'PORT': 5432,
-        'TEST_NAME': 'unittests',
+        'TEST': {
+            'NAME': 'unittests',
+        }
     }
 }
 
