43346d56db576d3925dc0151c4ac5884ec14855d
sonlinux <alison.mukoma@yahoo.com>
2018-03-27 09:39:48 +0200

    fix duplication

--- django_project/base/tests/__init__.py
+++ django_project/base/tests/__init__.py
@@ -1,1 +1,3 @@
+# coding=utf-8
+"""Test for base App."""
 __author__ = 'timlinux'
--- django_project/base/tests/test_views.py
+++ django_project/base/tests/test_views.py
@@ -278,7 +278,6 @@ class TestOrganisationCreate(TestCase):
             'name': u'Test organisation creation'
         }
         post_data_2 = {
-
             'name': u'Test organisation creation two'
         }
         post_data_3 = {
@@ -359,25 +358,15 @@ class TestOrganisationCreateWithSuperuserPermissions(TestCase):
         # Test that user is actually loged in.
         self.assertTrue(loged_in)
 
-        post_data = {
-            'name': u'Test organisation creation'
-        }
-        post_data_2 = {
+        post_data_list = [
+            'Test organisation creation',
+            'Test organisation creation two',
+            'Test organisation creation three']
 
-            'name': u'Test organisation creation two'
-        }
-        post_data_3 = {
-            'name': u'Test organisation creation three'
-        }
-
-        response = client.post(reverse('create-organisation'), post_data)
-        self.assertEqual(response.status_code, 302)
-
-        response = client.post(reverse('create-organisation'), post_data_2)
-        self.assertEqual(response.status_code, 302)
-
-        response = client.post(reverse('create-organisation'), post_data_3)
-        self.assertEqual(response.status_code, 302)
+        for post_data in post_data_list:
+            response = client.post(reverse('create-organisation'),
+                                   {'name': post_data})
+            self.assertEqual(response.status_code, 302)
 
 
 class TestOrganisationCreateWithNoneStaffPermissions(TestCase):
@@ -404,9 +393,8 @@ class TestOrganisationCreateWithNoneStaffPermissions(TestCase):
 
     @override_settings(VALID_DOMAIN=['testserver', ])
     def test_oroganisation_create_with_none_staff_login(self):
-        """
-        Test that a none staff user can create an organisations.
-        """
+        """Test that a none staff user can create an organisations."""
+
         client = Client()
         loged_in = client.login(username='sonlinux', password='password')
 
@@ -437,7 +425,6 @@ class TestOrganisationCreateWithNoneStaffPermissions(TestCase):
             'name': u'Test organisation creation'
         }
         post_data_2 = {
-
             'name': u'Test organisation creation two'
         }
         post_data_3 = {
