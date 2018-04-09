c6a3b15e8fe19f12c2c576fc44e9aa14f8c66254
Ismail Sunni <imajimatika@gmail.com>
2018-04-09 09:29:25 +0700

    Add gml geometry parser.

--- django_project/geocontext/tests/test_utilities.py
+++ django_project/geocontext/tests/test_utilities.py
@@ -1,8 +1,13 @@
 # coding=utf-8
 """Test for utilities module."""
 
+import os
+
 from django.test import SimpleTestCase
-from geocontext.utilities import convert_coordinate
+from geocontext.utilities import convert_coordinate, parse_gml_geometry
+
+test_data_directory = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), 'data')
 
 
 class TestUtilities(SimpleTestCase):
@@ -13,3 +18,14 @@ class TestUtilities(SimpleTestCase):
         result = convert_coordinate(1, 1, 4326, 3857)
         self.assertAlmostEqual(result[0], 111319.49, places=2)
         self.assertAlmostEqual(result[1], 111325.14, places=2)
+
+    def test_parse_geometry_gml(self):
+        """Test parse_gml_geometry"""
+        gml_file_path = os.path.join(test_data_directory, 'wfs.xml')
+        self.assertTrue(os.path.exists(gml_file_path))
+        with open(gml_file_path) as file:
+            gml_string = file.read()
+        geom = parse_gml_geometry(gml_string)
+        self.assertIsNotNone(geom)
+        self.assertTrue(geom.valid)
+        self.assertEqual(geom.geom_type, 'Polygon')
--- django_project/geocontext/utilities.py
+++ django_project/geocontext/utilities.py
@@ -1,7 +1,13 @@
 # coding=utf-8
 """Utilities module for geocontext app."""
 
-from django.contrib.gis.geos import Point
+import logging
+from xml.dom import minidom
+
+from django.contrib.gis.geos import Point, GEOSGeometry
+from django.contrib.gis.gdal.error import GDALException
+
+logger = logging.getLogger(__name__)
 
 
 def convert_coordinate(x, y, epsg_source, epsg_target):
@@ -22,10 +28,32 @@ def convert_coordinate(x, y, epsg_source, epsg_target):
     :return: tuple of converted x and y in float.
     :rtype: tuple(float, float)
     """
-
     # create a geometry from coordinates
     point = Point(x, y, srid=epsg_source)
 
     point.transform(epsg_target)
 
     return point.x, point.y
+
+
+def parse_gml_geometry(gml_string):
+    """Parse geometry from gml document.
+
+    :param gml_string: String that represent full gml document.
+    :type gml_string: unicode
+
+    :returns: GEOGeometry from the gml document, the first one if there are
+        more than one.
+    :rtype: GEOSGeometry
+    """
+    xmldoc = minidom.parseString(gml_string)
+    try:
+        geometry_dom = xmldoc.getElementsByTagName('qgs:geometry')[0]
+        geometry_gml_dom = geometry_dom.childNodes[1]
+        return GEOSGeometry.from_gml(geometry_gml_dom.toxml())
+    except IndexError:
+        logger.error('No geometry found')
+        return None
+    except GDALException:
+        logger.error('GDAL error')
+        return None
