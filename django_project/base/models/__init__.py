bd86d12bf553daf06097fb17ab829d8ef9e1ea0c
ann26 <anita@kartoza.com>
2020-03-22 21:12:59 +0700

    add noqa for some lines

--- django_project/base/models/__init__.py
+++ django_project/base/models/__init__.py
@@ -1,5 +1,5 @@
 from base.models.project import *  # noqa
-from base.models.custom_domain import *
-from base.models.custom_flatpage import ProjectFlatpage
-from base.models.organisation import *
-from base.models.site_preferences import *
+from base.models.custom_domain import *  # noqa
+from base.models.custom_flatpage import *  # noqa
+from base.models.organisation import *  # noqa
+from base.models.site_preferences import *  # noqa
