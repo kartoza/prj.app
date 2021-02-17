606bae0ffae941174d691581ee30543527b8ecd2
suman <sumandari.ee@gmail.com>
2021-02-17 09:44:27 +0800

    support pagination for 'download all worksheet' feature

--- django_project/lesson/static/js/download_multiple_files.js
+++ django_project/lesson/static/js/download_multiple_files.js
@@ -20,7 +20,98 @@ function toggleCheckAllWorksheet(that) {
     }else {
         $checkbox.prop('checked', false)
     }
+    updatePaginationLink()
 }
+
+// ----------------------------------------------------------------------------
+// pagination support
+function getWorksheetListPage(){
+    // get the list of checked worksheet for pagination support
+    const urlParams = new URLSearchParams(window.location.search);
+    var page = urlParams.get('page');
+    if (page == null){
+        page = "1"change
+    }
+    var $checkedElements = $('.worksheet-checkbox:checked');
+    var worksheets = {};
+    $.each($checkedElements, function (index, element) {
+        let pk = $(element).attr('worksheet-pk');
+        let numbering = $(element).attr('worksheet-number');
+        worksheets[pk] = numbering
+    });
+    var result = {}
+    result[page] = worksheets
+    return result
+}
+
+function addWorksheetList(newWorksheet){
+    // merge worksheet dictionary for pagination support
+    // newWorksheet is a dictionary
+    const urlParams = new URLSearchParams(window.location.search);
+    const worksheet_all = urlParams.get('worksheet_all');
+    let worksheetQueryParam = JSON.parse(worksheet_all);
+    return  {...worksheetQueryParam, ...newWorksheet}
+}
+
+function updatePaginationLink(){
+    // add query parameter for pagination support
+    const urlParams = new URLSearchParams(window.location.search);
+    const worksheet_all = urlParams.get('worksheet_all');
+    let worksheetListPage = getWorksheetListPage();
+    let worksheetList = JSON.stringify(addWorksheetList(worksheetListPage));
+    let paginationElement = $('.pagination>li>a');
+    $.each(paginationElement, function (index, element) {
+        let href = $(element).attr('href');
+        if (worksheet_all !== '') {
+            href = href.replace(/&worksheet_all=.*/, '')
+        }
+        let newHref = `${href}&worksheet_all=${worksheetList}`;
+        element.href = newHref;
+    })
+}
+
+function updateCheckedWorksheet(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const worksheet_all = urlParams.get('worksheet_all');
+    var page = urlParams.get('page');
+    var parsedWorksheets = JSON.parse(worksheet_all);
+    for (let key in parsedWorksheets){
+        if (key == page) {
+            let worksheet = parsedWorksheets[key];
+            for (let pk in worksheet){
+                let $checkbox = $('[worksheet-pk='+ pk +']');
+                $checkbox.prop('checked', true);
+            }
+        }
+    }
+}
+
+// add to onLoad
+if(window.addEventListener) {
+    window.addEventListener('load', updatePaginationLink,false); //W3C
+    window.addEventListener('load', updateCheckedWorksheet,false); //W3C
+} else {
+    window.attachEvent('onload', updatePaginationLink); //IE
+    window.attachEvent('onload', updateCheckedWorksheet); //IE
+}
+
+function parseWorksheetAllList(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const worksheet_all = urlParams.get('worksheet_all');
+    const page = urlParams.get('page');
+    var parsedWorksheets = JSON.parse(worksheet_all);
+    var worksheets = {}
+    for (let key in parsedWorksheets){
+        if (key != page){
+            let worksheet = parsedWorksheets[key];
+            for (let k in worksheet){
+                worksheets[k] = worksheet[k]
+            }
+        }
+    }
+    return worksheets
+}
+//-----------------------------------------------------------------------------
 
 function downloadCheckedFiles() {
     var $checkedElements = $('.worksheet-checkbox:checked');
@@ -30,6 +121,10 @@ function downloadCheckedFiles() {
         var numbering = $(element).attr('worksheet-number');
         worksheets[pk] = numbering
     });
+
+    // get worksheet from another page
+    var parsingWorksheet = parseWorksheetAllList();
+    worksheets = {...worksheets, ...parsingWorksheet}
 
     window.open(worksheets_download_url + '?worksheet=' + JSON.stringify(worksheets))
 
