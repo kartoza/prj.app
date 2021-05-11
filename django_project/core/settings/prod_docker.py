4a1839066719b299f5609f7f0aa86d6cffca88df
suman <sumandari.ee@gmail.com>
2021-05-11 09:09:58 +0800

    update email setting

--- django_project/core/settings/prod_docker.py
+++ django_project/core/settings/prod_docker.py
@@ -3,6 +3,7 @@
 # noinspection PyUnresolvedReferences
 from .prod import *  # noqa
 import os
+import ast
 print(os.environ)
 
 DEBUG = False
@@ -32,12 +33,16 @@ DATABASES = {
 #
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # Host for sending e-mail.
-EMAIL_HOST = 'smtp'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp')
 # Port for sending e-mail.
-EMAIL_PORT = 25
+EMAIL_PORT = ast.literal_eval(os.environ.get('EMAIL_PORT', '25'))
 # SMTP authentication information for EMAIL_HOST.
 # See fig.yml for where these are defined
-EMAIL_HOST_USER = 'noreply@kartoza.com'
-EMAIL_HOST_PASSWORD = 'docker'
-EMAIL_USE_TLS = False
-EMAIL_SUBJECT_PREFIX = '[PROJECTA]'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'noreply@kartoza.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'docker')
+EMAIL_USE_TLS = ast.literal_eval(os.environ.get('EMAIL_USE_TLS', 'False'))
+EMAIL_USE_SSL = ast.literal_eval(os.environ.get('EMAIL_USE_SSL', 'False'))
+EMAIL_SUBJECT_PREFIX = os.environ.get('EMAIL_SUBJECT_PREFIX', '[PROJECTA]')
+
+SERVER_EMAIL = os.environ.get('ADMIN_EMAIL', 'noreply@kartoza.com')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'noreply@kartoza.com')
