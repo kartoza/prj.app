68e6e0a21fe41e18c3db9cb822303f368b0295e9
Dimas Ciputra <dimas.ciputra@gmail.com>
2022-05-31 21:19:58 +0800

    Fix certificate email not sent

--- django_project/core/settings/prod_docker.py
+++ django_project/core/settings/prod_docker.py
@@ -2,6 +2,7 @@
 """Configuration for production server."""
 # noinspection PyUnresolvedReferences
 from .prod import *  # noqa
+import ast
 import os
 print(os.environ)
 
@@ -28,16 +29,20 @@ DATABASES = {
 }
 
 
-# See fig.yml file for postfix container definition
-#
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# Host for sending e-mail.
-EMAIL_HOST = 'smtp'
-# Port for sending e-mail.
-EMAIL_PORT = 25
-# SMTP authentication information for EMAIL_HOST.
-# See fig.yml for where these are defined
-EMAIL_HOST_USER = 'noreply@kartoza.com'
-EMAIL_HOST_PASSWORD = 'docker'
-EMAIL_USE_TLS = False
-EMAIL_SUBJECT_PREFIX = '[PROJECTA]'
+EMAIL_BACKEND = os.environ.get(
+    'EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = os.environ.get(
+    'EMAIL_HOST', 'smtp')
+EMAIL_HOST_USER = os.environ.get(
+    'EMAIL_HOST_USER', 'noreply@kartoza.com')
+EMAIL_HOST_PASSWORD = os.environ.get(
+    'EMAIL_HOST_PASSWORD', 'docker')
+EMAIL_PORT = os.environ.get(
+    'EMAIL_PORT', 'True')
+EMAIL_SUBJECT_PREFIX = os.environ.get(
+    'EMAIL_SUBJECT_PREFIX', '[PROJECTA]')
+EMAIL_USE_TLS = ast.literal_eval(os.environ.get(
+    'EMAIL_USE_TLS', 'True'))
+EMAIL_USE_SSL = ast.literal_eval(os.environ.get(
+    'EMAIL_USE_SSL', 'False'))
+
